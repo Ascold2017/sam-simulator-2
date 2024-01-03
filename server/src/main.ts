1649320c02a8ee68d5b6e4c98747b8de482bab3e
Ascold2017 <mihail.pogosyan96@gmail.com>
2024-01-03 21:34:56 +0400

    Create HttpServer

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,28 +1,51 @@
 import Engine from './core/Engine/Engine.ts';
 import { SAM } from './core/SAM/SAM.ts';
+import HttpServer from '#src/server/HttpServer.ts';
+import MISSIONS from '#src/assets/MISSIONS.ts';
+import FLIGHT_OBJECT_TYPES from '#src/assets/FLIGHT_OBJECT_TYPES.ts';
+import { load } from 'https://deno.land/std@0.210.0/dotenv/mod.ts';
+
+const env = await load();
 
 const engine = new Engine();
 const sam = new SAM(engine);
 
-const conn = Deno.listen({ port: 8001 });
-const httpConn = Deno.serveHttp(await conn.accept());
-const e = await httpConn.nextRequest();
-if (e) {
-	e.respondWith(new Response('Hello World'));
-}
+const httpServer = new HttpServer(8001);
+
+httpServer.addEndpoint('/missions', async (ctx) => {
+	await ctx.respondWith(new Response(JSON.stringify(MISSIONS)));
+});
+
+httpServer.addEndpoint('/flight-object-types', async (ctx) => {
+	await ctx.respondWith(new Response(JSON.stringify(FLIGHT_OBJECT_TYPES)));
+});
+
+httpServer.addEndpoint('/sam-settings', async (ctx) => {
+	await ctx.respondWith(new Response(JSON.stringify(env)));
+});
+
+httpServer.addEndpoint('/select-target', async (ctx) => {
+	//await ctx.respondWith(new Response(JSON.stringify(env)));
+});
+
+httpServer.addEndpoint('/unselect-target', async (ctx) => {
+	//await ctx.respondWith(new Response(JSON.stringify(env)));
+});
+
+httpServer.addEndpoint('/reset-targets', async (ctx) => {
+	//await ctx.respondWith(new Response(JSON.stringify(env)));
+});
+
+httpServer.addEndpoint('/launch-missile', async (ctx) => {
+	//await ctx.respondWith(new Response(JSON.stringify(env)));
+});
+
+httpServer.addEndpoint('/reset-missile', async (ctx) => {
+	//await ctx.respondWith(new Response(JSON.stringify(env)));
+});
+
 /*
 TODO
-
-HTTP:
-GET /missions
-GET /flight-object-types
-GET /sam-settings
-
-POST /select-target
-POST /unselect-target
-POST /reset-targets
-POST /launch-missile
-POST /reset-missile
 
 WS:
 RADAR_OBJECTS_UPDATE
