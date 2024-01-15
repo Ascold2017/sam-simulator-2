49753c5f6d9a16c8cedffcf28798f34ee1919f67
Ascold2017 <mihail.pogosyan96@gmail.com>
2024-01-15 13:37:46 +0400

    Implement 1/2 aim method; Implement oveloading of missiles

--- server/src/core/Engine/FlightObject/Missile.ts
+++ server/src/core/Engine/FlightObject/Missile.ts
@@ -3,8 +3,9 @@ import type Engine from '../Engine.ts';
 import Vector3D from '#core/Vector3D.ts';
 import BaseFlightObject from './BaseFlightObject.ts';
 import type Enemy from './Enemy.ts';
+import { IPoint } from '#src/core/Engine/Engine.ts';
 
-type GuidanceMethod = '3P' | '1/2';
+type GuidanceMethod = '3P' | '1/2' | '1';
 
 export default class Missile extends BaseFlightObject {
 	private readonly target: Enemy;
@@ -25,7 +26,6 @@ export default class Missile extends BaseFlightObject {
 		this.killRadius = Number(samParams['MISSILE_KILL_RADIUS']);
 		this.velocity = Number(samParams['MISSILE_VELOCITY']);
 		this.method = method;
-		console.log(method);
 	}
 
 	update(time: number): void {
@@ -41,25 +41,56 @@ export default class Missile extends BaseFlightObject {
 		const targetDistance = targetVector.sub(prevMissileVector)
 			.r() as number;
 
-		const hitPositionPreemtives = {
-			'3P': new Vector3D({ x: 0, y: 0, z: 0 }),
-			'1/2': new Vector3D({ x: 0, y: 0, z: 0 }), // TODO
+		const currentPositionHandler = {
+			'3P': () =>
+				this.calcMissilePosition(
+					targetVector,
+					prevMissileVector,
+					targetDistance,
+					dFlightDistance,
+				),
+			'1/2': () =>
+				this.calcMissilePosition(
+					this.calcPreemtiveVector(targetVector, targetDistance, 0.5),
+					prevMissileVector,
+					targetDistance,
+					dFlightDistance,
+				),
+			'1': () =>
+				this.calcMissilePosition(
+					this.calcPreemtiveVector(targetVector, targetDistance, 1),
+					prevMissileVector,
+					targetDistance,
+					dFlightDistance,
+				),
 		};
 
-		const preemtiveVector = hitPositionPreemtives[this.method];
-
-		const currentPosition = this.calcMissilePosition(
-			targetVector.add(preemtiveVector),
-			prevMissileVector,
-			targetDistance,
-			dFlightDistance,
-		);
+		const currentPosition = currentPositionHandler[this.method]();
 		this.currentPoint = {
 			x: currentPosition.x() as number,
 			y: currentPosition.y() as number,
 			z: currentPosition.z() as number,
 			v: this.velocity,
 		};
+
+		const prevMissileRotation = this.currentRotation;
+
+		this.currentRotation = Math.atan2(
+			this.currentPoint.y - (prevMissileVector.y() as number),
+			this.currentPoint.x - (prevMissileVector.x() as number),
+		);
+
+		const deltaRotation = Math.abs(
+			prevMissileRotation - this.currentRotation,
+		);
+
+		if (
+			this.traveledDistance > samParams.MIN_CAPTURE_RANGE &&
+			deltaRotation > samParams.MISSILE_MAX_DELTA_ROTATION
+		) {
+			this.destroy();
+			return;
+		}
 
 		if (targetDistance <= this.killRadius) {
 			this.target.kill();
@@ -92,5 +123,41 @@ export default class Missile extends BaseFlightObject {
 		const t1 = (-b - sqrt) / (2 * a);
 		const t2 = (-b + sqrt) / (2 * a);
 		return targetVector.scale(t1 > t2 ? t1 : t2);
+	}
+
+	private calcPreemtiveVector(
+		targetVector: Vector3D,
+		targetDistance: number,
+		scale: number,
+	) {
+		const radialVelocity = this.calcRadialVelocityOfTarget(
+			this.target.getCurrentPoint(),
+			this.target.getCurrentRotation(),
+		);
+
+		const timeToHit = targetDistance /
+			(this.velocity + radialVelocity);
+		const l = this.velocity * timeToHit * scale;
+
+		return new Vector3D({
+			x: l * Math.cos(this.target.getCurrentRotation()) +
+				(targetVector.x() as number),
+			y: l * Math.sin(this.target.getCurrentRotation()) +
+				(targetVector.y() as number),
+			z: targetVector.z() as number,
+		});
+	}
+
+	private calcRadialVelocityOfTarget(
+		targetPosition: IPoint,
+		targetRotation: number,
+	) {
+		const targetAzimuth = Math.atan2(targetPosition.y, targetPosition.x);
+		// Angle between azimut to flight object and rotation of flight object
+		const targetAngle =
+			(targetAzimuth > targetRotation
+				? targetAzimuth - targetRotation
+				: targetRotation - targetAzimuth) - Math.PI;
+		return targetPosition.v * Math.cos(targetAngle);
 	}
 }
