b0db22d847c77d74fa233ec4d6be3df6d317acf1
Mihail Pogosyan <mihail.pogosyan96@gmail.com>
2024-07-19 16:42:54 +0400

    Create start screen

--- sam-simulator-app/src/App.vue
+++ sam-simulator-app/src/App.vue
@@ -3,6 +3,14 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-
+<div class="app-layout">
   <RouterView />
+</div>
 </template>
+
+<style scoped>
+.app-layout {
+  @apply h-screen bg-cover bg-center bg-black bg-opacity-50;
+  background-image: url('/background.jpg');
+}
+</style>
--- sam-simulator-app/src/views/StartScreen.vue
+++ sam-simulator-app/src/views/StartScreen.vue
@@ -1,10 +1,22 @@
 <template>
-  <div >
-    <h1>Start</h1>
+  <div class="start-screen">
+      <h1 class="start-screen__title">SAM Simulator</h1>
 
-    <ul>
-      <li v-for="mission in missionStore.missions" :key="mission.id" @click="launchMission(mission.id)">{{ mission.name }}</li>
-    </ul>
+      <div class="start-screen__content">
+        <h2 class="start-screen__subtitle">Миссии</h2>
+        <ul class="start-screen__mission-list">
+          <li v-for="mission in missionStore.missions" :key="mission.id" @click="launchMission(mission.id)"
+            class="start-screen__mission-item">
+            <img src="https://via.placeholder.com/150" alt="Mission Image" class="start-screen__mission-image"/>
+            <span class="start-screen__mission-name">{{ mission.name }}</span>
+          </li>
+        </ul>
+
+        <router-link to="/mission-editor"
+          class="start-screen__link">
+          Редактор миссий
+        </router-link>
+      </div>
   </div>
 </template>
 
@@ -26,3 +38,42 @@ function launchMission(missionId: number) {
   router.push({ name: 'game' })
 }
 </script>
+
+<style scoped>
+.start-screen {
+ @apply flex flex-col items-center justify-center h-full;
+}
+
+.start-screen__title {
+  @apply text-4xl font-bold text-white mb-8;
+}
+
+.start-screen__content {
+  @apply bg-white bg-opacity-75 rounded-lg shadow-lg p-6 w-3/4 lg:w-1/2;
+}
+
+.start-screen__subtitle {
+  @apply text-2xl font-semibold mb-4;
+}
+
+.start-screen__mission-list {
+  @apply list-none p-0;
+}
+
+.start-screen__mission-item {
+  @apply flex items-center p-2 border-b last:border-b-0 cursor-pointer hover:bg-gray-200;
+}
+
+.start-screen__mission-image {
+  @apply w-16 h-16 mr-4;
+}
+
+.start-screen__mission-name {
+  @apply text-lg;
+}
+
+
+.start-screen__link {
+  @apply mt-6 inline-block text-blue-500 hover:text-blue-700;
+}
+</style>
